087c1eb88fb72d97b3589a57b33b518a5ba50d9d
Lucca Torrezilha Soares <luccatsoares@gmail.com>
2022-11-21 12:16:51 -0300

    compara pelo numero de matricula

--- picture.py
+++ picture.py
@@ -1,15 +1,18 @@
 import face_recognition as fr
 from engine import reconhece_rosto, getRostos
 
-desconhecido = reconhece_rosto("./img/desconhecido.JPG")
+def register_face(matricula):
 
-if desconhecido[0]:
-    rosto_desconhecido = desconhecido[1][0]
-    rostos_conhecidos, nome_rostos = getRostos()
-    resultados = fr.compare_faces(rostos_conhecidos, rosto_desconhecido)
-    print(resultados)
+    desconhecido = reconhece_rosto(f"{matricula}.png")
 
-    for i in range(len(nome_rostos)):
-        resultado = resultados[i]
-        if(resultado):
-            print(f"Rosto do {nome_rostos} foi reconhecido")
+    if desconhecido[0]:
+        rosto_desconhecido = desconhecido[1][0]
+        rostos_conhecidos, nome_rostos = getRostos()
+        resultados = fr.compare_faces(rostos_conhecidos, rosto_desconhecido)
+        print(resultados)
+
+        for i in range(len(nome_rostos)):
+            resultado = resultados[i]
+            if(resultado):
+                print(f"Rosto do {matricula} foi reconhecido")
+    return
