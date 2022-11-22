54a48cbb3d25af97570f1ebb0e1d218400d30ef8
Lucca Torrezilha Soares <luccatsoares@gmail.com>
2022-11-22 00:27:02 -0300

    teste

--- picture.py
+++ picture.py
@@ -9,7 +9,7 @@ def register_face(matricula):
         rosto_desconhecido = desconhecido[1][0]
         rostos_conhecidos, nome_rostos = getRostos()
         resultados = fr.compare_faces(rostos_conhecidos, rosto_desconhecido)
-        print(resultados)
+        print("resultados = ",resultados)
 
         for i in range(len(nome_rostos)):
             resultado = resultados[i]
